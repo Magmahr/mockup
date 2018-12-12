ca08a725eba22583ed480ca5d0eabbfebf609a64
matt maher <mattmaher0@gmail.com>
2018-12-12 14:31:46 -0500

    added files

---
+++ index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+document.addEventListener("click", e => {
+
+
+})
+
+document.addEventListener("mouseover", e => {
+    if (e.target.id === 'p2') {
+      pitch2.className = "hover"
+    }
+})
+
+})
